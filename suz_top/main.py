b4a6369e8b7f980a112ee19a2b9c6e13c87c75ca
VitoCorleona <85547988+VitoCorleona@users.noreply.github.com>
2021-07-09 22:14:42 +0500

    update

--- suz_top/main.py
+++ suz_top/main.py
@@ -5,17 +5,27 @@ Created on Thu Jul  8 16:52:10 2021
 @author: Toshmurodov Ilkhomjon
 """
 import random as r
+def play():
+    suzlar = ["gilam","qoshiq","vilka","kosa","piyola"]
+    suz = r.choice(suzlar)
+    chalkash_suz = list(suz)
+    r.shuffle(chalkash_suz)
+    print (f"Bu qanday so'z: {chalkash_suz}")
+    return suz
 
-suzlar = ["gilam","qoshiq","vilka","kosa","piyola"]
-suz = r.choice(suzlar)
-chalkash_suz = list(suz)
-r.shuffle(chalkash_suz)
 
-javob = input(f"Bu qanday so'z: {chalkash_suz}\n>>>")
-if suz == javob:
-    print(f"Bu haqiqatdan {suz.title()} edi.") 
-else:
-    print("Qayta urinib ko'ring.")
+
+
+
+
+
+
+
+#javob = input(f"Bu qanday so'z: {chalkash_suz}\n>>>")
+#if suz == javob:
+   # print(f"Bu haqiqatdan {suz.title()} edi.") 
+#else:
+    #print("Qayta urinib ko'ring.")
         
 
 
